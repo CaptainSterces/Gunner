47020561723ea39e164f904d1444332207b1ac60
CaptainSterces <CaptainSterces@gmail.com>
2022-07-06 14:56:02 -0300

    Enemy Spawn System
    Close enemies are hard to hit.
    Need to fix that

---
+++ Gunner/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private float timeStamp = 3f;
+    
+    public float coolDown = 0.2f;
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (timeStamp <= Time.time)
+        {
+            timeStamp = Time.time + coolDown;
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+        }
+        
+    }
+}
---
+++ Gunner/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private int randomPoint;
+    private Vector3 offset;
+    private int randomZ;
+    private Vector3 flyPoint;
+    public ShipMove movePoints;
+    public int speed = 10;
+
+    void Start()
+    {
+        movePoints = GameObject.Find("Ship").GetComponent <ShipMove>();
+        randomPoint = Random.Range(0, 22);
+        randomZ = Random.Range(-20, 20);
+        offset = new Vector3(0, 0, randomZ);
+        flyPoint = new Vector3(0,0,0);
+        flyPoint += (movePoints.moveTarget[randomPoint].transform.position) + offset;
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(flyPoint);
+        transform.position = Vector3.MoveTowards(transform.position, flyPoint, speed * Time.deltaTime);
+        if(transform.position == flyPoint)
+        {
+            Destroy(this);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+    }
+}
